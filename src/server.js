712ee3d71e6d1ff16190b947b5e9990da66407d5
Вадим <vadim@MacBook-Air-Vadim.local>
2024-12-12 11:13:59 +0200

    Fix

--- src/server.js
+++ src/server.js
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Указываем статическую папку для HTML, CSS и JS
 app.use(express.static(path.join(__dirname)));
 
-// Обслуживаем index.html для всех запросов
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Запускаем сервер
 app.listen(PORT, () => {
     console.log(`Сервер запущен: http://localhost:${PORT}`);
 });
